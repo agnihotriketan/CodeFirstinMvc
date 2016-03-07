f80979fe38a3001f8ce53d899c1fe4a4fc93487d
unknown <kagnihotri@Crux.internal.tavisca.com>
2016-03-07 16:58:22 +0530

    First

---
+++ CodeFirstinMvc/CodeFirstinMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeFirstinMvc.Startup))]
+namespace CodeFirstinMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
